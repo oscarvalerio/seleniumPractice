8cba9a8ad292e90f2435759e675cd11f4a06556e
oscarvalerio <oscarvalerio@gmail.com>
2022-07-12 20:59:18 -0500

    Updating header of class 1 file

--- seleniumTraining/src/test/java/practice1.java
+++ seleniumTraining/src/test/java/practice1.java
@@ -1,3 +1,7 @@
+//#######################################################################
+// questions about the code? email to oscarvalerio@gmail.com
+//#######################################################################
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
